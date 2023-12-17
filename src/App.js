6189e73a820b3532d04782b7ffd9ff21a0335992
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2023-12-18 01:03:01 +0530

    Components and Containers Files Created

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Article, Brand, Cta, Navbar, Feature } from './Components';
+import { Blog, Footer, Possibility, WhatGPT, Header, Features } from './Container';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Container/whatGPT/WhatGPT.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatGPT.css'
+
+const WhatGPT = () => {
+  return (
+    <div>
+      WhatGPT
+    </div>
+  )
+}
+
+export default WhatGPT
