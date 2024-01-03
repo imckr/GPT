78cef39f6a1c06f8857924f1a9b2ad085f98ee02
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2024-01-03 14:27:57 +0530

    Project Completed

--- src/Container/footer/Footer.jsx
+++ src/Container/footer/Footer.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import './footer.css';
+import gptLogo from  '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="gpt__footer section__padding">
+      <div className="gpt__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt__footer-links">
+        <div className="gpt__footer-links__logo">
+          <img src={gptLogo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+
+        <div className="gpt__footer-links__div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contacts</p>
+        </div>
+        <div className="gpt__footer-links__div">
+          <h4>Company</h4>
+          {/* <p>Overons</p> */}
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt__footer-links__div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          {/* <p>Counters</p> */}
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="gpt__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
